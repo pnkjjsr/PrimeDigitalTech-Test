16e65bd2358a0762c68fb0386c493ab60854995b
pnkjjsr <pnkj_jsr@yahoo.co.in>
2016-08-22 13:40:06 +0530

    Commit a new reposatery

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('primeDigitalTechTest')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('primeDigitalTechTest', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'toastr']);
+
+})();
